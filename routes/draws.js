ba65795b976236e3af4641feb25894bc5b0c1fe9
Ivan Wu <ivan.wu@fairfaxmedia.com.au>
2014-09-12 17:50:23 +1000

    coding for inserting data
    making progress for inserting data to mongodb

--- routes/draws.js
+++ routes/draws.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 var fs = require('fs');
+var csv = require("fast-csv");
 var csvPath = 'public/resources/LottoSaturday.csv';
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://localhost/lottosaturday');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -14,27 +15,98 @@ db.once('open', function callback () {
 });
 
 var Schema = mongoose.Schema;
-
-var usersSchema = new Schema({
-    name: String
+var drawsSchema = new Schema({
+    drawid: String,
+    drawdate: Number,
+    winning: [Number],
+    supplementary: [Number]
 });
 
-
-var users = mongoose.model('users', usersSchema);
+//var usersSchema = new Schema({
+//    name: String
+//});
+//
+//
+//var users = mongoose.model('users', usersSchema);
+//var testUser = new users(
+//    {name: 'testname7' }
+//);
+//
+//testUser.save(function(err, testUser) {
+//    if (err) return console.error(err);
+//    console.dir(testUser);
+//});
 //console.log(db);
 
-/* GET users listing. */
+
+/* GET Lotto Satuarday Draws listing. */
+/* root start from /draws */
 router.get('/', function(req, res) {
 
-    mongoose.model('users').find(function(err, users){
-         res.send(users);
-    });
-    //res.render('draws', { title: 'Draws' });
+//    mongoose.model('users').find(function(err, users){
+//        console.log(users);
+//        res.send(users);
+//    });
 
-    //var rs = fs.createReadStream(csvPath);
-    //var ws = fs.createWriteStream('public/resources/ws.csv');
-    //rs.pipe(ws);
-    //rs.pipe(res);
+    var draws = mongoose.model('draws', drawsSchema);
+    draws.findOne({ 'drawid': '3453'}, function(err){
+        if (err) return handleError(err);
+    });
+    //console.log(draws.find());
+//    res.send(function(){
+//        draws.findOne({ drawid: '3453'}, function(err){
+//            if (err) return handleError(err);
+//        });
+//    });
+
+
+//    var rs = fs.createReadStream(csvPath);
+//    var ws = fs.createWriteStream('public/resources/ws.csv');
+//    rs.pipe(ws);
+//    rs.pipe(res);
+
+});
+
+router.get('/build', function(req, res) {
+
+    var draws = mongoose.model('drawsdraws', drawsSchema);
+
+    csv.fromPath(csvPath)
+        .on("record", function(data,i) {
+//            console.log(data);
+//            if(data[0] > 3000){
+//                console.log(data[0] + " " + data[1] + " " + data[2] + " " + data[3] + " " + data[4] + " " + data[5]+ " " + data[6]+ " " + data[7] + " " + data[8] + " " + data[9]);
+//            }
+
+            if (i != 0) {
+
+                if (data[8] == null) data[8] = 0;
+
+                var draw = new draws({
+                    drawid: data[0],
+                    drawdate: data[1],
+                    winning: [
+                        data[2], data[3], data[4], data[5], data[6]
+                    ],
+                    supplementary: [
+                        data[7], data[8]
+                    ]
+                });
+
+                draw.save(function (err, draw) {
+                    if (err) {
+                        console.dir(draw);
+                        console.error(err);
+                    }
+                    console.dir(draw);
+                });
+            }
+
+        })
+        .on("end", function(){
+            console.log("######################### done #########################");
+            res.send('All draws has been imported into database successfully');
+        });
 
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET Home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
+
+/* GET Users page. */
+router.get('/users', function(req, res) {
+  res.render('users', { title: 'Users' });
+});
+
+
+/* GET Draws page. */
+//router.get('/draws', function(req, res) {
+//  res.render('draws', { title: 'Draws' });
+//});
 
 module.exports = router;
